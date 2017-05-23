d85053c4741e175842c1538f64316b74535b1610
Mike Smart <mike.smart@jadu.co.uk>
2017-05-23 22:35:42 +0100

    add jest and remove redundant babel config

--- tests/utils/fs.utils.test.js
+++ tests/utils/fs.utils.test.js
@@ -1,5 +1,5 @@
-const { describe, it, beforeEach, afterEach } = require('mocha')
-const { expect } = require('chai')
+require('jest')
+
 const util = require('../../utils/fs.utils')
 const path = require('path')
 const fs = require('fs')
@@ -17,11 +17,11 @@ describe('fs utils', () => {
     })
 
     describe('deepWriteSync()', () => {
-        it('should recursively create directories that do not exist', () => {
+        test('should recursively create directories that do not exist', () => {
             const file = path.join(base, 'foo/bar/foo.txt')
 
             util.deepWriteSync(file, 'Hello, World!')
-            expect(fs.existsSync(file)).to.be.true
+            expect(fs.existsSync(file)).toBeTruthy()
         })
     })
 })
